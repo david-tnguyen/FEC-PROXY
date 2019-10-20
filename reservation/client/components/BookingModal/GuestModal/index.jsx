63c06b261d5dc2fbfdbe88a11bd257ad50ba08bc
David Nguyen <tnguyen.david@gmail.com>
2019-10-20 13:14:01 -0700

    Complete css refactor for guest modal

--- reservation/client/components/BookingModal/GuestModal/index.jsx
+++ reservation/client/components/BookingModal/GuestModal/index.jsx
@@ -1,38 +1,25 @@
 import React from 'react';
+import styles from './guestModal.scss';
 
 const GuestModal = () => (
-  <div id="guest-modal-container">
-    <div className="guest-modal-spacing">
-      <div className="guest-modal-spacing">
-        <div className="guest-modal-table">
-          <div className="guest-modal-cell">
-            <div className="guest-modal-label">Adults</div>
-          </div>
-        </div>
+  <div className={styles.wrapper}>
+    <div className={styles.compact}>
+      <div className={styles.compact}>
+        <div className={styles.title}>Adults</div>
       </div>
 
-      <div className="guest-modal-spacing">
-        <div className="guest-modal-table">
-          <div className="guest-modal-cell">
-            <div className="guest-modal-label">Children</div>
-            <div className="guest-modal-label">Ages 2-12</div>
-          </div>
-        </div>
+      <div className={styles.compact}>
+        <div className={styles.title}>Children</div>
+        <div className={styles.title}>Ages 2-12</div>
       </div>
 
-      <div className="guest-modal-spacing">
-        <div className="guest-modal-table">
-          <div className="guest-modal-cell">
-            <div className="guest-modal-label">Infants</div>
-            <div className="guest-modal-label">Under 2</div>
-          </div>
-        </div>
+      <div className={styles.compact}>
+        <div className={styles.title}>Infants</div>
+        <div className={styles.title}>Under 2</div>
       </div>
 
-      <div id="guest-modal-bottom">
-        <div id="guest-modal-info">
-          3 guests maximum. Infants don’t count toward the number of guests.
-        </div>
+      <div className={styles.warning}>
+        3 guests maximum. Infants don’t count toward the number of guests.
       </div>
     </div>
   </div>
