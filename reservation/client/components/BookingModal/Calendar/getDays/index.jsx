89c7ba48f32ca3bb6ca132d0a18dacb555bf8ea6
David Nguyen <tnguyen.david@gmail.com>
2019-10-20 23:59:28 -0700

    Complete getDays css

--- reservation/client/components/BookingModal/Calendar/getDays/index.jsx
+++ reservation/client/components/BookingModal/Calendar/getDays/index.jsx
@@ -3,6 +3,7 @@ import moment from 'moment';
 import getFirstDayOfMonth from '../../../../utils/getFirstDayOfMonth';
 import getDaysInMonth from '../../../../utils/getDaysInMonth';
 import classNames from 'classnames';
+import styles from './getDays.scss';
 
 export default (now, blockedDates, startDate, endDate, today, checkoutDate, onDaySelect, onDateRangeSelect) => {
   const daysInMonth = [];
@@ -25,13 +26,12 @@ export default (now, blockedDates, startDate, endDate, today, checkoutDate, onDa
       isBlocked = true;
     }
 
-    const dayClass = classNames({
-      'calendar-day': true,
-      'blocked-day': isBlocked,
-      'active-day': !isBlocked && !startDate || dateSelected < startDate && dateSelected !== endDate,
-      'start-date-select': !isBlocked && dateSelected.isSame(startDate),
-      'date-range-span': !isBlocked && !isOutsideRange && startDate && dateSelected > startDate && dateSelected <= endDate,
-      'date-range-span-selected': !isBlocked && !isOutsideRange && dateSelected > startDate && dateSelected <= checkoutDate,
+    const dayClass = classNames(styles['calendar-day'], {
+      [styles['blocked-day']]: isBlocked,
+      [styles['active-day']]: !isBlocked && !startDate || dateSelected < startDate && dateSelected !== endDate,
+      [styles['start-date-select']]: !isBlocked && dateSelected.isSame(startDate),
+      [styles['date-range-span']]: !isBlocked && !isOutsideRange && startDate && dateSelected > startDate && dateSelected <= endDate,
+      [styles['date-range-span-selected']]: !isBlocked && !isOutsideRange && dateSelected > startDate && dateSelected <= checkoutDate,
     });
 
     daysInMonth.push(
