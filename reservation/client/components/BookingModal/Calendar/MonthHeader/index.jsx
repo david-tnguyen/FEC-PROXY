fd7cdd00bb275b9c7b5cf7b2f11cbe68fb99df59
David Nguyen <tnguyen.david@gmail.com>
2019-10-21 19:52:49 -0700

    Complete css refactor for month header

--- reservation/client/components/BookingModal/Calendar/MonthHeader/index.jsx
+++ reservation/client/components/BookingModal/Calendar/MonthHeader/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import getCurrentMonth from '../../../../utils/getCurrentMonth';
 import getCurrentYear from '../../../../utils/getCurrentYear';
+import styles from './monthHeader.scss';
 
 const MonthHeader = (props) => (
-  <div id='current-period'>
+  <div className={styles.monthHeader}>
     <strong>{getCurrentMonth(props.now)} {getCurrentYear(props.now)}</strong>
   </div>
 );
