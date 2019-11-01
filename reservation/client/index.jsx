a14e1a5c58e4adf395533499fbac94744e4598eb
David Nguyen <tnguyen.david@gmail.com>
2019-10-31 20:41:31 -0700

    init redux store

--- reservation/client/index.jsx
+++ reservation/client/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import Reservation from './components/Footer';
 require('./styles/style.scss');
 
